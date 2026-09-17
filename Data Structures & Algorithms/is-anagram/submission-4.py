3f026169b99a69baa80a19eaf0e567bb613145f3
IAMamontov <iamamontov@yandex.ru>
2026-09-17 13:48:20 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if set(s) != set(t):
+            return False
+        freq_dict_s, freq_dict_t = {}, {}
+        for char in set(s):
+            freq_dict_s[char] = s.count(char)
+            freq_dict_t[char] = s.count(char)
+        if freq_dict_s != freq_dict_t:
+            return False
+        return True
+        
