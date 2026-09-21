0852e952274d9b4d6a839bdde2baa352c49942f9
IAMamontov <iamamontov@yandex.ru>
2026-09-21 13:38:02 +0300

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        additions_dict = {}
+        for i in range(len(nums)):
+            addition = target - nums[i]
+            if addition in nums[i:]:
+                if addition in additions_dict:
+                    return [additions_dict[addition], nums.index(addition, i)]
+                additions_dict[addition] = nums.index(addition)
+        print(additions_dict)
+        for i in range(len(nums)):
+            if target - nums[i] in additions_dict and target - nums[i] !=  nums[i]:
+                return [i, additions_dict[target - nums[i]]]
+        
